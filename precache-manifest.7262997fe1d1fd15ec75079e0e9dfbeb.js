51e1d27ab1ef96fb24a84976de936816e13ed471
m.iqabgohar1122 <m.iqanngohar@gmail.com>
2020-02-29 12:21:37 +0500

    Updates

---
+++ precache-manifest.7262997fe1d1fd15ec75079e0e9dfbeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cf8b2673c917150f0d394c8b6d1447",
+    "url": "/react-layoot-build/index.html"
+  },
+  {
+    "revision": "898c7fbaf74f7b6c24d1",
+    "url": "/react-layoot-build/static/css/2.5e99816f.chunk.css"
+  },
+  {
+    "revision": "f64715e21972e806abfe",
+    "url": "/react-layoot-build/static/css/main.13df56fc.chunk.css"
+  },
+  {
+    "revision": "898c7fbaf74f7b6c24d1",
+    "url": "/react-layoot-build/static/js/2.6aa1a79e.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/react-layoot-build/static/js/2.6aa1a79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64715e21972e806abfe",
+    "url": "/react-layoot-build/static/js/main.21be3aa1.chunk.js"
+  },
+  {
+    "revision": "8fc0c50852c8013f5e93",
+    "url": "/react-layoot-build/static/js/runtime-main.ebbdb5c9.js"
+  },
+  {
+    "revision": "c21058230699e9501475774eb3da3a6d",
+    "url": "/react-layoot-build/static/media/b1.c2105823.jpg"
+  }
+]);
